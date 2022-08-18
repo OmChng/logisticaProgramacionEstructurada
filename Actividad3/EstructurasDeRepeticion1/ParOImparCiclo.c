f769ff184a3beea6441edf19cf4ffbf0ce01afdc
Omar Chenge <44788682+OmChng@users.noreply.github.com>
2022-08-17 19:39:04 -0500

    Actividad 3
    Actividad 3 terminada

--- Actividad3/EstructurasDeRepeticion1/ParOImparCiclo.c
+++ Actividad3/EstructurasDeRepeticion1/ParOImparCiclo.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+
+int main (){
+  int i = 1, n, num;
+  printf("\nESTRUCTURAS DE REPETICIÓN 1\n");
+  printf("\nPor favor ingrese la cantidad de números que quiere analizar: ");
+  scanf("%10d", &n);
+  if(n == 0) {
+    printf("\nGracias por usar mi programa.\n");
+    return 0;
+  }
+  do{
+    printf("\nPor favor ingrese el %dº número que quiere analizar: ", i);
+    scanf("%d", &num);
+    while(num == 0){
+      printf("El número a analizar debe ser mayor que cero.");
+      printf("\nPor favor ingrese el %dº número que quiere analizar: ", i);
+      scanf("%d", &num);
+    }
+    if(num %2 == 0) printf("\nEl número '%d' es par.\n", num);
+    else printf("\nEl número '%d' es impar.\n", num);
+    i++;
+  } while (i <= n);
+  printf("Todos sus números fueron analizados.");
+  printf("\nGracias por usar mi programa.\n");
+  return 0;
+}
--- Actividad3/EstructurasDeRepeticion2/TotalAPagar.c
+++ Actividad3/EstructurasDeRepeticion2/TotalAPagar.c
@@ -1,1 +1,34 @@
 #include <stdio.h>
+
+int main(){
+  int i = 1, j, k, l, m, n = 0;
+  printf("\nESTRUCTURAS DE REPETICIÓN 2\n");
+  printf("\nPor favor ingrese cuántos tipos de diferentes artículos lleva en el carrito: ");
+  scanf("%5d", &j);
+  if(j <= 0){
+    printf("\nSu carrito está vacío! No debe de pagar nada.");
+    printf("\nGracias por usar mi programa.\n");
+    return 0;
+  } else {
+    for(k = 0; k < j; k++){
+      printf("\n¿Cuántas unidades del %dº artículo lleva?: ", k + 1);
+      scanf("%5d", &l);
+      while(l == 0){
+        printf("\nNo puede llevar %d unidades de un artículo, es ilógico.", l);
+        printf("\n¿Cuántas unidades del %dº artículo lleva?: ", k + 1);
+        scanf("%5d", &l);
+      }
+      printf("\n¿Cuánto cuesta una unidad del %dº artículo?: ", k + 1);
+      scanf("%5d", &m);
+      while(m == 0){
+        printf("\nVenga, ni que regalaran las cosas en esta tienda...");
+        printf("\n¿Cuánto cuesta una unidad del %dº artículo?: ", k + 1);
+        scanf("%5d", &m);
+      }
+      n += l*m;
+    }
+    printf("\nUsted debe de pagar: $%d pesos\n", n);
+    printf("\nGracias por usar mi programa.\n");
+  }
+  return 0;
+}
